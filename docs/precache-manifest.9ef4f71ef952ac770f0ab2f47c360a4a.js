e9a402d497ce69318a00efff6a5761f16f2e1345
jelenaandj <jelena.andjelkovic453@gmail.com>
2020-05-25 14:00:13 +0200

    build

---
+++ docs/precache-manifest.9ef4f71ef952ac770f0ab2f47c360a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59332d4088b9b933717d4463b74796e8",
+    "url": "/Budget-App/index.html"
+  },
+  {
+    "revision": "8dda08d7afdc8121b69a",
+    "url": "/Budget-App/static/css/main.f5bcd9d5.chunk.css"
+  },
+  {
+    "revision": "8a8c42baa5fcb720a2cc",
+    "url": "/Budget-App/static/js/2.8af5db9b.chunk.js"
+  },
+  {
+    "revision": "b51715a5f03753299d4efccc59b99a20",
+    "url": "/Budget-App/static/js/2.8af5db9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8dda08d7afdc8121b69a",
+    "url": "/Budget-App/static/js/main.1262fe9b.chunk.js"
+  },
+  {
+    "revision": "d642c9cd358230f6793b",
+    "url": "/Budget-App/static/js/runtime-main.0d637b08.js"
+  }
+]);
